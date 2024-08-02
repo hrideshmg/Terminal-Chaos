61c15a7cef734d793aad05b159f4d42a17ac8561
hridesh <hridesh699@gmail.com>
2024-08-02 22:44:34 +0530

    update part 2

---
+++ Arrakis-dex/Eolian Caves/Entrance3/01/01L/01/02/01/02/01/Citadel/KharnokTheBloodForged.py
@@ -0,0 +1,14 @@
+import subprocess
+import platform
+
+system = platform.system()
+if system == "Linux":
+    popen = subprocess.Popen(
+        [".linuxDist/dist/Kharnok the BloodForged/Kharnok the BloodForged"]
+    )
+    popen.wait()
+elif system == "Darwin":
+    popen = subprocess.Popen([".macDist/dist/Khanrock-final"])
+    popen.wait()
+elif system == "Windows":
+    print("What are you doing on windows?")
